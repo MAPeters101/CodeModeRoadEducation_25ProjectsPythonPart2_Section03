f0ff3badf2ebd96c7bc71f51887efb7ac9c0b338
Mark Peters <mapeters101@earthlink.net>
2025-01-08 23:32:29 -0500

    Work on lesson011 from 3:15 to 3:34.

--- lesson011.py
+++ lesson011.py
@@ -9,3 +9,10 @@ string2 = input("Enter a string: ")
 ind = int(input("What index do you want to add this to: "))
 list.insert(ind, string2)
 print(list)
+
+ind = int(input("What index do you want to change: "))
+el = input("Enter the new element: ")
+list[ind] = el
+print(list)
+
+
