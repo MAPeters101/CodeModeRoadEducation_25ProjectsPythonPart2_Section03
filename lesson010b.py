1f99a6cb8095258e3dc2d64f245b2b82b005c80b
Mark Peters <mapeters101@earthlink.net>
2025-01-08 23:21:02 -0500

    Work on lesson010b from 4:20 to 5:45.

---
+++ lesson010b.py
@@ -0,0 +1,12 @@
+animals = ["anteater", "bear", "cat", "dog", "eagle", "fish", "giraffe", "hippo", "iguana"]
+
+
+for i in range(len(animals)):
+    print(animals[i])
+
+for i in range(len(animals)):
+    print(animals[i], end=' ')
+
+for i in range(2, 6):
+    print(animals[i], end=' ')
+
