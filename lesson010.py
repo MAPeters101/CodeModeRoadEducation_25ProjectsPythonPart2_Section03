ea53f01c507ee984776e079190e80b70148dcb53
Mark Peters <mapeters101@earthlink.net>
2025-01-08 23:17:35 -0500

    Work on lesson010 to 4:20.

---
+++ lesson010.py
@@ -0,0 +1,8 @@
+animals = ["anteater", "bear", "cat", "dog", "eagle", "fish", "giraffe", "hippo", "iguana"]
+
+print(animals[2])
+print(animals[6])
+print(animals[-1])
+print(animals[:3])
+print(animals[4:5])
+print(animals[6:])
