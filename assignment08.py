5f72c677a4d41abb1b62a9eda883d555b4d8dabf
Mark Peters <mapeters101@earthlink.net>
2025-01-10 19:35:41 -0500

    Complete work assignment08.

--- assignment08.py
+++ assignment08.py
@@ -8,19 +8,19 @@ def display_phrase(answer, guesses):
     for word in answer:
         if word in guesses:
         # Append word to the 'display' list
-
+            display.append(word)
         else:
-    # Append the following to the 'display' list: "_" * len(word)
-
+        # Append the following to the 'display' list: "_" * len(word)
+            display.append("_" * len(word))
     return " ".join(display)
 
 
 def guess_the_phrase(phrase):
     # Create an empty list to store the guessed words
-    guessed_words =
+    guessed_words = []
 
     # Set the amount of incorrects that the player gets
-    incorrects =
+    incorrects = 5
 
     print("Welcome to the Guess the Phrase Game!\n")
 
@@ -29,21 +29,24 @@ def guess_the_phrase(phrase):
 
     while incorrects > 0:
         # Ask the user for ONE word (their guess)
-        user_guess =
+        user_guess = input("Please enter one word: ")
 
         if user_guess in phrase and user_guess not in guessed_words:
             # Append user guess to the 'guessed_words' list
-
+            guessed_words.append(user_guess)
             print("Congratulations! You guessed a correct word.")
-            print()  # Inside, call the display_phrase function
-
+            print(display_phrase(phrase_to_guess, guessed_words))  # Inside, call the display_phrase function
         else:
             # Decrement their incorrects by 1
+            incorrects -= 1
             print(f"Incorrect guess. Incorrects left: {incorrects}")
-            print()  # Inside, call the display_phrase function
+            print(display_phrase(phrase_to_guess, guessed_words))  # Inside, call the display_phrase function
 
         # Check if the length of 'guessed_words' equals length of 'phrase'
         # If so, congratulate the user AND break from the loop
+        if len(guessed_words) == len(phrase_to_guess):
+            print("CONGRATULATIONS!! You guessed the phrase!")
+            break
 
         if incorrects == 0:
             print("Sorry, you've run out of attempts. The correct phrase was:")
@@ -51,5 +54,5 @@ def guess_the_phrase(phrase):
 
 
 # Main Program
-phrase_to_guess = []  # Add a bunch of random words that form a sentence
+phrase_to_guess = ["I", "am", "really", "enjoying", "learning", "to", "program", "in", "python"]  # Add a bunch of random words that form a sentence
 guess_the_phrase(phrase_to_guess)
